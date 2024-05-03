4a738379be6787a6e55ad5869a829e3c508840f6
Maeve322 <maevepro28@mail.ru>
2024-05-03 14:23:51 +0300

    Make sql to build tables and fill data from csv

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
--- SQL-команды для создания таблиц
+
+-- Использование базы данных "north"
+\c north;
+
+-- Создание таблицы "employees"
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    birth_date DATE NOT NULL,
+    notes VARCHAR(500) NOT NULL
+);
+
+-- Создание таблицы "customers"
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    company_name VARCHAR(50) NOT NULL,
+    contact_name VARCHAR(50) NOT NULL
+);
+
+-- Создание таблицы "orders"
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id VARCHAR REFERENCES customers(customer_id),
+    employee_id INT REFERENCES employees(employee_id),
+    order_date DATE,
+    ship_city VARCHAR(255)
+);-- SQL-команды для создания таблиц
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import pandas as pd
+conn = psycopg2.connect(
+    dbname="north",
+    user="jasey",
+    password="jasey",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+# Загрузка данных из файла north_data.csv
+employees = pd.read_csv('employees_data.csv')
+customers = pd.read_csv('customers_data.csv')
+orders = pd.read_csv('orders_data.csv')
+# Запись данных в таблицу в базе данных
+for row in employees.itertuples(index=False, name=None):
+    cursor.execute("INSERT INTO employees VALUES %s", (row,))
+
+for row in customers.itertuples(index=False, name=None):
+    cursor.execute("INSERT INTO customers VALUES %s", (row,))
+
+for row in orders.itertuples(index=False, name=None):
+    cursor.execute("INSERT INTO orders (order_id,customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s,%s)", (row[0], row[1], row[2], row[3],row[4]))
+# Подтверждение изменений и закрытие курсора и соединения
+conn.commit()
+cursor.close()
+conn.close()
